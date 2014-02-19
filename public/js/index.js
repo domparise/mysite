7eb6d430d9b61f9e92ad8a562109dd29dc1cdaf5
Dom Parise <domparise@gmail.com>
2014-02-19 04:48:50 -0500

    holy shit that header looks good

--- public/js/index.js
+++ public/js/index.js
@@ -1,89 +1,49 @@
-var k = kontext( document.querySelector( '.kontext' ) );
-
-
-// API METHODS:
-
-// k.prev(); // Show prev layer
-// k.next(); // Show next layer
-// k.show( 3 ); // Show specific layer
-// k.getIndex(); // Index of current layer
-// k.getTotal(); // Total number of layers
-
-
-document.addEventListener( 'keyup', function( event ) {
-	if( event.keyCode === 37 ) k.prev();
-	if( event.keyCode === 39 ) k.next();
-}, false );
-
-var touchX = 0;
-var touchConsumed = false;
-
-document.addEventListener( 'touchstart', function( event ) {
-	touchConsumed = false;
-	lastX = event.touches[0].clientX;
-}, false );
-
-document.addEventListener( 'touchmove', function( event ) {
-	event.preventDefault();
-
-	if( !touchConsumed ) {
-		if( event.touches[0].clientX > lastX + 10 ) {
-			k.prev();
-			touchConsumed = true;
-		}
-		else if( event.touches[0].clientX < lastX - 10 ) {
-			k.next();
-			touchConsumed = true;
-		}
-	}
-}, false );
-
-
 $(document).ready( function () {
-	setTimeout( function() {
-		$('#hdr').animate({
+	$('#hdr').animate({
+		opacity: 1,
+		color: '#FFFFFF',
+		backgroundColor: '#00B2FF'
+	}, 1500);
+	setTimeout( function () {
+		$('.nav').animate({
 			opacity: 1,
-			backgroundColor: "#FFFFFF",
-			borderBottomColor: "#888888"
-		}, 1500);
+			color: '#FFFFFF',
+		}, 2000);
+		$('.navd').animate({
+			opacity: 1,
+			borderBottomColor: '#FF9923'
+		}, 2000);
 	}, 500);
 });
 $('.nav').on('click', function (event) {
 	$('.nav').removeClass('navd');
 	$(this).addClass('navd');
+	$('.navd').css('border-bottom-color','#FFFFFF');
 });
 $('#hdr').on('mouseenter', function () {
-	// console.log(event);
-	$(this).stop();
-	$(this).animate({
+	$(this).stop().animate({
 		opacity: 1,
-		color: "#000000",
-		backgroundColor: "#FFFFFF",
-		borderBottomColor: "#888888"
+		color: '#FFFFFF',
+		backgroundColor: '#00B2FF'
+	}, 500);
+	$('.nav').stop().animate({
+		opacity: 1,
+		color: '#FFFFFF'
 	}, 500);
 });
 $('#hdr').on('mouseleave', function () {
-	// console.log(event);
-	$(this).stop();
-	$(this).animate({
-		// opacity: 0.25,
-		color: "#FFFFFF",
-		backgroundColor: "rgba(240,240,240,0)",
-		borderBottomColor: "rgba(240,240,240,0)"
+	$(this).stop().animate({
+		color: '#FF9923',
+		backgroundColor: '#FFFFFF'
+	}, 500);
+	$('.nav').stop().animate({
+		opacity: 0.25,
+		color: '#00B2FF'
+	}, 500);
+	$('.navd').stop().animate({
+		opacity: 0.75,
+		color: '#00B2FF',
+		borderBottomColor: '#FF9923'
 	}, 500);
 });
-
-
-$('#nav-about').on('click', function (event) {
-	k.show(0);
-});
-
-$('#nav-tut').on('click', function (event) {
-	k.show(1);
-});
-
-$('#nav-proj').on('click', function (event) {
-	k.show(2);
-});
-
 
