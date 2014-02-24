e0cd10729afb5cf9a9ea04bf920ec88720171e33
Dom Parise <domparise@gmail.com>
2014-02-24 05:14:57 -0500

    got a pretty nice look goin on

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,7 @@
 $(document).ready( function () {
-	$('#hdr').animate({
+	$('#projects').hide();
+	$('#tutorials').hide();
+	$('.edge').animate({
 		opacity: 1,
 		color: '#FFFFFF',
 		backgroundColor: '#00B2FF'
@@ -13,38 +15,66 @@ $(document).ready( function () {
 			opacity: 1,
 			borderBottomColor: '#FF9923'
 		}, 2000);
+		$('#about').animate({
+			opacity: 1
+		}, 2000);
 	}, 500);
 });
+
 $('.nav').on('click', function (event) {
 	$('.nav').removeClass('navd');
 	$(this).addClass('navd');
 	$('.navd').css('border-bottom-color','#FFFFFF');
+	var navTo = $(this).get(0).id;
+	$('section').stop('sections',true,false).animate({opacity:0},500,function(){
+		$('section').hide();
+		if (navTo === 'nav-about') {
+			$('#about').show().animate({opacity:1},500);
+		} else if (navTo === 'nav-tut') {
+			$('#tutorials').show().animate({opacity:1},500);
+		} else if (navTo === 'nav-proj') {
+			$('#projects').show().animate({opacity:1},500);
+		}
+	});
 });
-$('#hdr').on('mouseenter', function () {
-	$(this).stop().animate({
+
+
+$('header').on('mouseenter', function () {
+	$('.edge').stop('color',true,false).animate({
 		opacity: 1,
-		color: '#00B2FF',
 		backgroundColor: '#007CB2'
 	}, 500);
-	$('.nav').stop().animate({
+	$('.nav').stop('color',true,false).animate({
 		opacity: 1,
 		color: '#FFFFFF'
 	}, 500);
 });
-$('#hdr').on('mouseleave', function () {
-	$(this).stop().animate({
+$('header').on('mouseleave', function () {
+	$('.edge').stop('color',true,false).animate({
 		opacity: 1,
-		color: '#FF9923',
 		backgroundColor: '#00B2FF'
-	}, 500);
-	$('.nav').stop().animate({
+	}, 500, 'easeOutQuad');
+	$('.nav').stop('color',true,false).animate({
 		opacity: 0.50,
 		color: '#FFFFFF'
 	}, 500);
-	$('.navd').stop().animate({
+	$('.navd').stop('color',true,false).animate({
 		opacity: 1,
-		color: '#FFFFFF',
 		borderBottomColor: '#FF9923'
 	}, 500);
+});
+
+
+$('footer').on('mouseenter', function () {
+	$(this).stop().animate({
+		top: '-=7.5%',
+		height: '+=7.5%'
+	},250);
+});
+$('footer').on('mouseleave', function () {
+	$(this).stop().animate({
+		height: '2.5%',
+		top: '97.5%'
+	},250);
 });
 
