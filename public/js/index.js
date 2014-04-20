1b3812d9bc53ca4ea6ad17ec2d488b9f21e83b7f
domparise@gmail.com <domparise@gmail.com>
2014-04-20 17:04:47 -0400

    committing for fun

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,90 @@
 $(document).ready( function () {
-	bindHoverWithBox( $('#about .content:eq(0)') );
-	bindHoverWithBox( $('#about .content:eq(1)') );
-	bindHoverWithBox( $('#about .content:eq(2)') );
+	var hash = window.location.hash.toLowerCase();
+	console.log(hash);
+	if (hash === '') hash = '#about';
+	$(hash).show();
+	$('#nav-'+hash.substr(1)).addClass('navd');
+	$('.edge').animate({
+		opacity: 1,
+		backgroundColor: '#61B1B8'
+	}, 1500);
+	setTimeout( function () {
+		$('.nav').animate({
+			opacity: 0.30
+			// opacity: 1
+		}, 2000);
+		$('.navd,'+hash).animate({
+			opacity: 1,
+			borderBottomColor: '#DC8930'
+		}, 2000);
+	}, 500);
+	$('body').animate({
+		backgroundColor: '#FAF6E0'
+	}, 15000);
 });
+
+$('.nav').on('click', function (event) {
+	$('.nav').removeClass('navd');
+	$(this).addClass('navd');
+	$('.navd').css('border-bottom-color','#FFFFFF');
+	var navTo = $(this).get(0).id;
+	window.location.hash = '#'+navTo.substr(4);
+	$('section').stop('sections',true,false).animate({opacity:0},500,function(){
+		$('#'+navTo.substr(4)).show().animate({opacity:1},500);
+	});
+});
+
+/* opacity of non-selected nav pieces */
+var transitionTime = 400;
+$('header').on('mouseenter', function () {
+	$('.nav').stop().animate({
+		opacity: 1
+	}, transitionTime);
+});
+$('header').on('mouseleave', function () {
+	$('header').stop().animate({
+		opacity: 1,
+	}, transitionTime);
+	$('.nav').stop().animate({
+		opacity: 0.30,
+	}, transitionTime);
+	$('.navd').stop().animate({
+		opacity: 1,
+		borderBottomColor: '#DC8930'
+	}, transitionTime);
+});
+
+
+function bindHover (selector) {
+	console.log('bind');
+	var i = 0,
+	interval = 0,
+	offset = selector.offset().top;
+	selector.on('mouseenter',function () {
+		console.log('hovered');
+		clearInterval(interval);
+		interval = setInterval(function() {
+			offset -= i/50;
+			selector.offset({top:offset});
+			selector.css('box-shadow','0 '+((10/25)*i)+'px '+((5/25)*i)+'px rgba(0,0,0,'+((0.35/25)*i)+'), 0 0 '+((20/25)*i)+'px rgba(0,0,0,'+((0.1/25)*i)+')');
+			if(i>=25){
+				clearInterval(interval);
+				j = 25;
+			}
+			else i += 1;
+		},1);
+	});
+	selector.on('mouseleave',function() {
+		clearInterval(interval);
+		interval = setInterval(function() {
+			offset += i/50;
+			selector.offset({top:offset});
+			selector.css('box-shadow','0 '+((10/25)*i)+'px '+((5/25)*i)+'px rgba(0,0,0,'+((0.35/25)*i)+'), 0 0 '+((20/25)*i)+'px rgba(0,0,0,'+((0.1/25)*i)+')');
+			if(i<=0) clearInterval(interval);
+			else i -= 1;
+		},1);
+	});
+}
 
 /*
 	also consider writing onclick functionality for hovered content paragraphs, which will persist the box content,
@@ -25,6 +107,7 @@ var j=0,
 	inView = [];
 
 function bindHoverWithBox (selector) {
+	console.log('bindw/box');
 	var i = 0,
 	interval = 0,
 	offset = selector.offset().top,
@@ -87,3 +170,14 @@ $('#about .content:eq(2)').on('mouseenter', function() {
 	$('#ktpWallpaper').stop().animate({opacity:0},function(){transComplete=true;});
 });
 
+window.onload = function () {
+	console.log('loaded');
+	bindHover( $('#javascript') );
+	bindHover( $('#webdev') );
+	bindHover( $('#nodejs') );
+
+	bindHoverWithBox( $('#abt-right p:eq(0)') );
+	bindHoverWithBox( $('#abt-right p:eq(1)') );
+	bindHoverWithBox( $('#abt-right p:eq(2)') );
+
+};
